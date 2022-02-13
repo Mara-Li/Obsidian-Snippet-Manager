ef3665eb0466d8ab7e58fa9237cf42551f77fc4c
Mara <mara-li@outlook.fr>
2022-02-13 16:16:45 +0100

    ci(executable): Fix Setup

--- setup_cx.py
+++ setup_cx.py
@@ -3,11 +3,19 @@ import os
 from cx_Freeze import Executable, setup
 
 base = None
+folder='Windows'
 if sys.platform == "win32":
     base = "Win32GUI"
+    folder = "Windows"
+elif sys.platform.startswith('linux'):
+    folder='Linux'
+elif sys.platform.startswith('darwin'):
+    folder='MacOS'
+option_folder = os.path.join('app', folder)
 
 menu = os.path.join('obs_manager', 'src', 'menu.py')
 ico = os.path.join('obs_manager', 'GUI', 'hand.ico')
+option = { 'build_exe': option_folder}
 executables = [
     Executable(
         menu, base=base, icon=ico
